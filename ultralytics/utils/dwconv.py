3c2d26a41b45ff4e03ee2cffa2e35f771a20a438
Zahra Alipour <138664659+ZahraAlipour703@users.noreply.github.com>
2025-06-30 18:57:48 +0330

    Update dwconv.py
    Signed-off-by: Zahra Alipour <138664659+ZahraAlipour703@users.noreply.github.com>

--- ultralytics/utils/dwconv.py
+++ ultralytics/utils/dwconv.py
@@ -1,13 +1,21 @@
-import torch
 import torch.nn as nn
 from .autopad import autopad
 
 class DWConv(nn.Module):
-  def __init__(self, c1, c2, k = 3, s = 1, p = None, act = True):
-    super().__init__()
-    swlf.dw = nn.Conv2d(c1, c2, k, s, autopad(k, p), groups = c1, bias = False)
-    swlf.pw = nn.Conv2d(c1, c2, 1, 1, 0, bias = False)
-    self.bn = nn.BatchNorm2d(c2)
-    self.act = nn.SiLU() if act else nn.Identity()
+    """
+    Depthwise + pointwise convolution block.
+    """
+    def __init__(self, c1, c2, k=3, s=1, p=None, act=True):
+        super().__init__()
+        # Depthwise conv (groups = c1)
+        self.dw = nn.Conv2d(c1, c1, k, s, autopad(k, p), groups=c1, bias=False)
+        # Pointwise conv
+        self.pw = nn.Conv2d(c1, c2, 1, 1, 0, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = nn.SiLU() if act else nn.Identity()
+
     def forward(self, x):
-      return self.act(self.bn(self.pw(slef.dw(x))))
+        x = self.dw(x)
+        x = self.pw(x)
+        x = self.bn(x)
+        return self.act(x)
